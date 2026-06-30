93bb170425291506cba240868d62b82dbad3d6c4
sonaligosawi <gosawi.sonali@gmail.com>
2026-06-30 14:47:53 +0200

    created "ItemRow" view to display cutom rows in list.

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Sonali Gosawi on 30/06/2026.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemRow()
+}

05e4bd2653b636f9a4ac3a6fd5471613877f02e7
sonaligosawi <gosawi.sonali@gmail.com>
2026-07-02 10:57:09 +0200

    Added ItemDetail view and display detail view on tapping each menu item.

---
+++ iDine/ItemDetail.swift
@@ -0,0 +1,18 @@
+//
+//  Itemdetail.swift
+//  iDine
+//
+//  Created by Sonali Gosawi on 02/07/2026.
+//
+
+import SwiftUI
+
+struct Itemdetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Itemdetail()
+}

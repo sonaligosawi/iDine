c1ab81a74210e6b0e3dd64a0c0f7f47221fc55e9
sonaligosawi <gosawi.sonali@gmail.com>
2026-07-03 16:31:40 +0200

    Created OrderView UI.

---
+++ iDine/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Sonali Gosawi on 03/07/2026.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderView()
+}

d07f6a674e6f133d6fdce65df2ca52e3764aed8d
sonaligosawi <gosawi.sonali@gmail.com>
2026-07-04 17:36:47 +0200

    Created "CheckoutView".

---
+++ iDine/Views/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Sonali Gosawi on 04/07/2026.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
